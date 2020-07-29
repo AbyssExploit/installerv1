3ab801ec4aeb199e37bdddb84f18dcd0f76f855f
AbyssExploit <63506758+AbyssExploit@users.noreply.github.com>
2020-07-29 22:32:24 +0700

    Add files via upload

---
+++ abyss.sh
@@ -0,0 +1,221 @@
+#!/system/xbin/bash
+
+
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+
+figlet Abyss Walker | lolcat
+figlet Mix Tools | lolcat
+
+
+
+echo -e " Welcome To Mix Tools Instagram" | lolcat
+echo -e " Author    : Abyss Walker" | lolcat
+echo -e " Youtube   : Abyss Walker" | lolcat
+echo -e " Instagram : abysswalker260" | lolcat
+
+echo -e " ∆ Silahkan Pilih Tools Yang Ingin Kalian Install ∆" | lolcat
+
+echo -e "__________________________________" | lolcat
+echo -e "1.Tools Instagram Hack (Metode Brute Force)" | lolcat
+echo -e "__________________________________" | lolcat
+echo -e "2.Tools Instagram Bot" | lolcat
+echo -e "__________________________________" | lolcat
+echo -e "3.Tools Inshackle (Autofollowers etc)" | lolcat
+echo -e "__________________________________" | lolcat
+echo -e "4.Tools Instagram Brute Force (Tested Work 100%)" | lolcat
+echo -e "__________________________________" | lolcat
+echo -e "5.Tools Instagram (Followers,Download Stories etc)" | lolcat
+echo -e "__________________________________" | lolcat
+echo -e "6.Tools Instagram Followers Scraper" | lolcat
+echo -e "__________________________________" | lolcat
+echo -e "7.Tools Auto Report Instagram" | lolcat
+echo -e "__________________________________" | lolcat
+echo -e "8.Tools Private Tools Instagram v2.5" | lolcat
+echo -e "__________________________________" | lolcat
+echo -e "9.Tools Instagram Bot v2 " | lolcat
+echo -e "__________________________________" | lolcat
+echo -e "10.Tools Instagram Bot v3 " | lolcat
+echo -e "0.Keluar" | lolcat
+read -p "Root@AbyssWalker ~# " asu
+
+
+if [ $asu = 1 ] || [ $asu = 1 ]
+then
+clear
+figlet Darknet | lolcat
+echo "Untuk Tutorial Silahkan Comment Atau Join Grup"
+sleep 3
+apt update
+apt upgrade
+apt install git
+git clone https://github.com/storiku/instahack
+mv instahack $HOME
+cd $HOME/instahack
+bash ig.sh
+fi
+
+
+if [ $asu = 2 ] || [ $asu = 2 ]
+then
+clear
+figlet Darknet | lolcat
+echo "Untuk Tutorial Silahkan Comment Atau Join Grup"
+sleep 3
+apt update
+apt upgrade
+apt install git
+apt install python2
+git clone https://github.com/verluchie/instabot.py
+mv  instabot.py $HOME
+cd $HOME/instabot.py
+pip2 install -r requirements.txt
+python2 example.py
+fi
+
+if [ $asu = 3 ] || [ $asu = 3 ]
+then
+clear
+figlet Darknet | lolcat
+echo -e "Auto Install" | lolcat
+echo "Untuk Tutorial Silahkan Comment Atau Join Grup"
+sleep 3
+apt update
+apt upgrade
+apt install git
+apt install bash
+git clone https://github.com/xd20111/inshackle
+mv inshackle $HOME
+cd $HOME/inshackle
+bash inshackle.sh
+fi
+
+if [ $asu = 4 ] || [ $asu = 4 ]
+then
+clear
+figlet Darknet | lolcat
+echo "Untuk Tutorial Silahkan Comment Atau Join Grup"
+sleep 3
+apt update
+apt upgrade
+apt install git
+apt install python
+git clone https://github.com/Pure-L0G1C/Instagram
+mv Instagram $HOME
+cd $HOME/Instagram
+pip install -r requirements.txt
+pip install bs4
+python instagram.py
+fi
+
+if [ $asu = 5 ] || [ $asu = 5 ]
+then
+clear
+figlet Darknet | lolcat
+echo "Untuk Tutorial Silahkan Comment Atau Join Grup"
+sleep 3
+apt update
+apt upgrade
+apt install git
+git clone https://github.com/rixon-cochi/Lucifer.git
+mv Lucifer $HOME
+cd $HOME/Lucifer
+bash setup.sh
+bash instacracker.sh
+fi
+
+if [ $asu = 6 ] || [ $asu = 6 ]
+then
+figlet Darknet | lolcat
+echo "Untuk Tutorial Silahkan Comment Atau Join Grup"
+sleep 3
+apt install git
+git clone  https://github.com/nOOBIE-nOOBIE/Instagram-Follower-Scraper
+apt update
+apt upgrade
+pkg install python
+mv Instagran-Follower-Scraper $HOME
+cd $HOME/Instagram-Followers-Scraper
+pip install -r requirements.txt
+python instabot.py
+fi
+
+if [ $asu = 7 ] || [ $asu = 7 ]
+then
+figlet Darknet | lolcat
+echo "Untuk Tutorial Silahkan Comment Atau Join Grup"
+sleep 3
+apt update
+apt upgrade
+apt install git
+apt install python
+git clone https://github.com/mrwn007/007spam-BOT.git
+mv 007spam-BOT $HOME
+cd $HOME/007spam-BOT
+pip install requests
+python bot.py
+fi
+
+if [ $asu = 8 ] || [ $asu = 8 ]
+then
+figlet Darknet | lolcat
+echo "Untuk Tutorial Silahkan Comment Atau Join Grup"
+sleep 3
+apt update
+apt upgrade
+apt install unzip -y
+pkg install nodejs-lts -y
+git clone https://github.com/officialputuid/toolsig.git
+mv toolsig $HOME
+cd $HOME/toolsig
+unzip lib.zip
+node index.js
+fi
+
+if [ $asu = 9 ] || [ $asu = 9 ]
+then
+figlet Darknet | lolcat
+echo "Untuk Tutorial Silahkan Comment Atau Join Grup"
+sleep 3
+apt update
+apt upgrade
+apt install python
+apt install git
+git clone https://github.com/N74NK/taig
+mv taig $HOME
+cd $HOME/taig
+pip install -r requirements.txt
+python igbot.py
+fi
+
+if [ $asu = 10 ] || [ $asu = 10 ]
+then
+figlet Darknet | lolcat
+echo "Untuk Tutorial Silahkan Comment Atau Join Grup"
+sleep 3
+apt update
+apt upgrade
+pkg install nodejs-lts -y
+git clone https://github.com/kumpulanremaja/ig
+mv ig $HOME
+cd $HOME/ig
+unzip node_modules.zip
+npm install
+node index.js
+fi
+
+if [ $asu = 0 ] || [ $asu = 0 ]
+then
+echo -e "Abyss Walker" | lolcat
+echo -e "No System Is Safe" | lolcat
+echo -e "Be Carrefuly On Internet" | lolcat
+echo -e "Malware Everywhere" | lolcat
+echo -e "Thanks For Use This Tools" | lolcat
+exit
+fi
